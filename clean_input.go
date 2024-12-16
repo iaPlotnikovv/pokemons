c1c1c10531234b13d6937fae355d2ba1754b6d1a
iaPlotnikovv <idoffix@gmail.com>
2024-12-16 07:43:03 +0300

    added REPL in main & improved clean_input

--- clean_input.go
+++ clean_input.go
@@ -5,7 +5,7 @@ import "strings"
 func CleanInput(text string) []string {
 	//The purpose of this function is to split the users input into "words" based on whitespace.
 	//It should also lowercase the input and trim any leading or trailing whitespace.
-	result := strings.Split(strings.ToLower(text), " ")
+	result := strings.Fields(strings.ToLower(text))
 
 	return result
 }
--- main.go
+++ main.go
@@ -1,7 +1,23 @@
 package main
 
-import "fmt"
+import (
+	"bufio"
+	"fmt"
+	"os"
+)
 
 func main() {
-	fmt.Println("Hello World!")
+	//fmt.Println("Hello World!")
+	scanner := bufio.NewScanner(os.Stdin)
+	for {
+		fmt.Print("\n Pokemon >")
+		if scanner.Scan() {
+
+			input := CleanInput(scanner.Text())
+
+			fmt.Printf("\nYour command was: %v\n", input[0])
+
+		}
+	}
+
 }
